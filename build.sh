92460d251fe8fc48eaceb5f35ec55d37ec9cc689
skobicj <julian@skobic.eu>
2025-01-19 18:32:23 +0100

    Added FLutter dev and key

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ set -ouex pipefail
 # Add EduVPN
 curl -O https://app.eduvpn.org/linux/v4/rpm/app+linux@eduvpn.org.asc
 rpm --import app+linux@eduvpn.org.asc
-cat << 'EOF' | sudo tee /etc/yum.repos.d/python-eduvpn-client_v4.repo
+cat << 'EOF' | tee /etc/yum.repos.d/python-eduvpn-client_v4.repo
 [python-eduvpn-client_v4]
 name=eduVPN for Linux 4.x (Fedora $releasever)
 baseurl=https://app.eduvpn.org/linux/v4/rpm/fedora-$releasever-$basearch
@@ -24,3 +24,6 @@ dnf install eduvpn-client
 wget "https://repo.protonvpn.com/fedora-$(cat /etc/fedora-release | cut -d' ' -f 3)-unstable/protonvpn-beta-release/protonvpn-beta-release-1.0.2-1.noarch.rpm"
 dnf install ./protonvpn-beta-release-1.0.2-1.noarch.rpm && check-update --refresh -y
 dnf install proton-vpn-gnome-desktop -y
+
+# Add Flutter Dev Dependencies
+dnf install ninja-build clang cmake pkg-config gtk3-devel xz-devel xz-libs libstdc++-devel -y
